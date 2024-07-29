5676eb7a08f67cf27811ebdee3de4b241ba1bab2
Joni Susanto <riskhiakbar@gmail.com>
2024-07-29 11:27:39 +0700

    Laravel Version 11

---
+++ resources/views/home/header.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--=============== FAVICON ===============-->
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/x-icon">
+
+    <!--=============== BOXICONS ===============-->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
+    <!--=============== SWIPER CSS ===============-->
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
+
+    <!--=============== CSS ===============-->
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+
+    <title> Responsive portfolio website Ansel </title>
+</head>
+<body>
